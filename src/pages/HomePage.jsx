099ca878e14f8afc9610dc5591b96ee7420f5703
Georgios <test@example.com>
2021-01-13 23:54:05 +0100

    shows all customers in api

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,64 @@
+import React, {useState, useEffect} from 'react';
+import CustomerListItem from '../components/CustomerListItem';
+import {Table} from "react-bootstrap"
+
+
+export default function HomePage() {
+    const [customerList, setCustomerList] = useState([])
+
+    useEffect( () => {
+        getCustomerList()
+    }, [])
+
+    function getCustomerList() {
+        const url = "https://frebi.willandskill.eu/api/v1/customers/"
+        const token = localStorage.getItem("CUSTOMERS")
+        fetch (url, {
+            headers: {
+                "Content-Type" : "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        })
+        .then(res => res.json())
+        .then(data => setCustomerList(data.results))
+            
+    }
+
+  return (
+    <div>
+      <h2>Home Page</h2>
+
+      <Table striped bordered hover className="table">
+        <thead>
+          <tr>
+            <th>Customer Name</th>
+            <th>Oraganisation number</th>
+            <th>Vat nr</th>
+          </tr>
+        </thead>
+        </Table>
+        
+        {/* <tbody>
+          <tr>
+            <td> */}
+      {customerList.map(item => {
+        return <CustomerListItem key={item.id} customerData={item} />
+      })}
+      
+      {/* </td>
+      <td>
+      {customerList.map(item => {
+        return <CustomerListItem key={item.organisationNr} customerData={item} />
+      })}
+      </td>
+      <td>
+      {customerList.map(item => {
+        return <CustomerListItem key={item.vatNr} customerData={item} />
+      })}
+      </td>
+      </tr>
+        </tbody>
+      </Table>*/}
+    </div> 
+  );
+}
